c61964233b64e8fb0d9c81bb05e1c705cdd51232
DawrAli33285 <lemightyeagle@gmail.com>
2024-11-30 16:25:36 +0500

    newupdates

--- src/components/sellercomponents/sellerbondlisting.js
+++ src/components/sellercomponents/sellerbondlisting.js
@@ -363,7 +363,7 @@ const filterItems = (value) => {
                                             <Link to={`/buyerpromisebonddetail/${bond?._id}`}>View</Link>
                                         </td>
                                               <td className={` border-gray-300 p-[10px] flex justify-center py-[10px] `}>
-                       {!alreadyForExchange?<p onClick={() => {
+                       {!bond?.alreadyForExchange?<p onClick={() => {
                                                 navigate(`/exchange?id=${bond?._id}`)
                                             }} target="_blank" className='cursor-pointer border-[1px] rounded-[20px] px-[20px] py-[10px] w-fit text-[#1DBF73]'>Register for Exchange</p>:null}
                                             <p onClick={(e) => {
@@ -415,7 +415,7 @@ const filterItems = (value) => {
                                             <Link to={`/buyerpromisebonddetail/${bond?._id}`}>View</Link>
                                         </td>
                                               <td className={` border-gray-300 p-[10px] flex justify-center py-[10px] `}>
-                    {!alreadyForExchange?                        <p onClick={() => {
+                    {!bond?.alreadyForExchange?                        <p onClick={() => {
                                                 navigate(`/exchange?id=${bond?._id}`)
                                             }} target="_blank" className='cursor-pointer border-[1px] rounded-[20px] px-[20px] py-[10px] w-fit text-[#1DBF73]'>Register for Exchange</p>:''}
                                             <p onClick={(e) => {
