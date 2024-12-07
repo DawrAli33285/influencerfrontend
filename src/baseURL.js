6552f7ffb82c781f4d23c6e3322d1a61506d72e8
DawrAli33285 <lemightyeagle@gmail.com>
2024-12-07 12:59:12 +0500

    newchanges

--- src/baseURL.js
+++ src/baseURL.js
@@ -1,3 +1,3 @@
-export const BASE_URL="http://localhost:5000"
-// export const BASE_URL="https://influencerbackend-956183099426.us-central1.run.app"
+// export const BASE_URL="http://localhost:5000"
+export const BASE_URL="https://influencerbackend-956183099426.us-central1.run.app"
     
