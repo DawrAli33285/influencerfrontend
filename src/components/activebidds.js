c065ae101afbb19b08f61b3c5f1dcf80b5581752
DawrAli33285 <lemightyeagle@gmail.com>
2024-12-08 12:21:03 +0500

    newchanges

--- src/components/activebidds.js
+++ src/components/activebidds.js
@@ -29,23 +29,39 @@ export default function ActiveBids({ state, loading }) {
                     <p className="lg:text-[0.94rem] text-[0.75rem] text-center lg:w-[70%] mx-auto">Discover the trusted issuers that power our platform, offering unmatched reliability and experties.Explore their unique features and benefits to find the perfect fit for your need</p>
 
                 </div>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[40px]">
-                    {state?.market?.length > 0 ? state?.market?.map((val, i) => {
-                        return <div className="bg-white  w-[342px]  flex flex-col gap-[20px] rounded-[20px] relative">
-                            <img src={val?.photos[0]
-                                ? val.photos[0].replace('http://localhost:5000', BASE_URL)
-                                : img} alt="cardimg"  className="rounded-[10px] w-[342px] h-[489px] object-cover" />
-                            <div className="absolute bg-[#0000003d] p-[20px] w-full h-full flex flex-col gap-[5px] rounded-[20px] justify-end">
-                                <p className="text-white lg:text-[1.25rem] text-[1rem] font-bold">{val?.issuer_id?.user_id?.username}</p>
-                                <p className="lg:text-[0.94rem] text-[0.75rem]  text-[#FFFFFFBF]">{val?.missions?.length > 0 ? val?.missions[0]?.description?.slice(0,50)+'...' : `No mission`}</p>
-                                <span className="lg:text-[1.25rem] text-[1rem] font-bold  text-white">${val?.bond_issuerance_amount}</span>
-                            </div>
-                        </div>
-                    }) : <div className="flex w-full justify-center items-center text-center">
-                        <p>No Record Found</p>
-                    </div>}
+                <div className="flex flex-wrap justify-start gap-[20px]">
+  {state?.market?.length > 0 ? state?.market?.map((val, i) => {
+    return (
+      <div className="bg-white flex-shrink-0 w-[100%] sm:w-[100%] md:w-[30%] lg:w-[30%] flex flex-col gap-[20px] rounded-[20px] relative">
+        <img
+          src={val?.photos[0]
+            ? val.photos[0].replace('http://localhost:5000', BASE_URL)
+            : img}
+          alt="cardimg"
+          className="rounded-[10px] w-full h-[489px] object-cover"
+        />
+        <div className="absolute bg-[#0000003d] p-[20px] w-full h-full flex flex-col gap-[5px] rounded-[20px] justify-end">
+          <p className="text-white lg:text-[1.25rem] text-[1rem] font-bold">
+            {val?.issuer_id?.user_id?.username}
+          </p>
+          <p className="lg:text-[0.94rem] text-[0.75rem] text-[#FFFFFFBF]">
+            {val?.missions?.length > 0
+              ? val?.missions[0]?.description?.slice(0, 50) + "..."
+              : `No mission`}
+          </p>
+          <span className="lg:text-[1.25rem] text-[1rem] font-bold text-white">
+            ${val?.bond_issuerance_amount}
+          </span>
+        </div>
+      </div>
+    );
+  }) : (
+    <div className="flex w-full justify-center items-center text-center">
+      <p>No Record Found</p>
+    </div>
+  )}
+</div>
 
-                </div>
                 <Link to="/search?filter=bond&search=" className="text-[#1DBF73] border-[#1DBF73] bg-[#1dbf7327] border rounded-[8px] w-fit px-[20px] py-[10px] mx-auto font-bold lg:text-[0.94rem] text-[0.75rem]">Explore The Market</Link>
 
             </>}
